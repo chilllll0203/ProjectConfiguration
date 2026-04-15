8732f1a50260d99c126937239b3fa5e44015a7a8
chilllll0203 <andrei02.03.2007@outlook.com>
2026-04-15 17:57:12 +0400

    Изменено

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ engine = create_async_engine('sqlite+aiosqlite:///database.db', echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
-# @router.post("/setup_database")
-# async def setup_database():
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.drop_all)
-#         await conn.run_sync(Base.metadata.create_all)
+@router.post("/setup_database")
+async def setup_database():
+    async with engine.begin() as conn:
+    await conn.run_sync(Base.metadata.drop_all)
+    await conn.run_sync(Base.metadata.create_all)
