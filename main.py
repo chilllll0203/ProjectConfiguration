d0766a6f25e1cfba4c9c294b93bfac30ac33f309
chilllll0203 <andrei02.03.2007@outlook.com>
2026-04-15 17:59:08 +0400

    Обновлено

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-from routers import auth, users, pages, database
+from routers import auth, users, pages
+import database
 
 app = FastAPI()
 
