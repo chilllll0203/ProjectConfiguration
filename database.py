6116816e6466091939926103cb9506b155c4009b
chilllll0203 <andrei02.03.2007@outlook.com>
2026-04-15 19:53:07 +0300

    изменено

--- database.py
+++ database.py
@@ -9,5 +9,5 @@ async_session = async_sessionmaker(engine, expire_on_commit=False)
 @router.post("/setup_database")
 async def setup_database():
     async with engine.begin() as conn:
-    await conn.run_sync(Base.metadata.drop_all)
-    await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
