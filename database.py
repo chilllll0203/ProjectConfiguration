8b837b9640b6659bffd7ac66c51d6f2075359c14
chilllll0203 <andrei02.03.2007@outlook.com>
2026-04-15 19:58:11 +0300

    изменено

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from models import Base
+from fastapi import APIRouter
 
 # Создание асинхронного движка и сессии
 engine = create_async_engine('sqlite+aiosqlite:///database.db', echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
+router = APIRouter()
 
 @router.post("/setup_database")
 async def setup_database():
