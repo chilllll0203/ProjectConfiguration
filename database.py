29312db209bebe565d3a6d2942bd426149e81b4b
chilllll0203 <andrei02.03.2007@outlook.com>
2026-04-15 15:08:16 +0300

    изменено

--- database.py
+++ database.py
@@ -1,12 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 # Создание асинхронного движка и сессии
-DATABASE_URL = "postgresql://postgres_projectconfiguration_user:acfTXKyGRhtVXIBhDCCJnkUQCG3Qrpwu@dpg-d7fnofd8nd3s73e196d0-a.virginia-postgres.render.com/postgres_projectconfiguration"
-
-engine = create_async_engine(
-    DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://"),
-    echo=True
-)
+engine = create_async_engine('sqlite+aiosqlite:///database.db', echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
